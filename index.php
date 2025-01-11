67664bf3963de23b4b64b55df17c8243f7e06691
Aryan Narayan <122514284+Aryan-788@users.noreply.github.com>
2025-01-11 21:18:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Discuss Project</title>
+    <title>Query-Connect Project</title>
     <?php  include('./client/commonFiles.php') ?>
 </head>
 <body>
